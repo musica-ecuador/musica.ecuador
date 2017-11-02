5770191052a7c9ff7acaea673f160355ea140a72
jcsuscriptor <jcsuscriptor@gmail.com>
2017-11-02 00:39:50 -0500

    Configuraciones por defecto

--- ETL/CONFIG.py
+++ ETL/CONFIG.py
@@ -7,8 +7,8 @@ PARSE_APPLICATION_ID = "123"
  	
 
 #spotify
-spotify_client_credentials_manager = SpotifyClientCredentials(client_id = "efb1585a452d4930af31ca24ee893910",
-                                   client_secret = "c28b1c21d3f54ffbbbdad1c33fc1d237")
+spotify_client_credentials_manager = SpotifyClientCredentials(client_id = "spotify_cliente_id",
+                                   client_secret = "spotify_cliente_secret")
 								   
 			
 #youtube
@@ -16,6 +16,6 @@ spotify_client_credentials_manager = SpotifyClientCredentials(client_id = "efb15
 from apiclient.discovery import build
 
 # arguments to be passed to build function
-YOUTUBE_DEVELOPER_KEY = "AIzaSyBHDCXMifi8vkHbcG79JIN0GlecpNkdf3c"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"		
+YOUTUBE_DEVELOPER_KEY = "YOUTUBE_DEVELOPER_KEY"
+YOUTUBE_API_SERVICE_NAME = "YOUTUBE_API_SERVICE_NAME"
+YOUTUBE_API_VERSION = "YOUTUBE_API_VERSION"		
