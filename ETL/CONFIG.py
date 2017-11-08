0cb252a9a5b3018b8237132f9fcdc8b7fdbac07e
jcsuscriptor <jcsuscriptor@gmail.com>
2017-11-07 22:00:45 -0500

    Cambiar claves de servicios (Api key)

--- ETL/CONFIG.py
+++ ETL/CONFIG.py
@@ -11,8 +11,8 @@ PARSE_SERVER =  "localhost:1337"
 PARSE_APPLICATION_ID = 123 	
 
 #spotify
-spotify_client_credentials_manager = SpotifyClientCredentials(client_id = "efb1585a452d4930af31ca24ee893910",
-                                   client_secret = "c28b1c21d3f54ffbbbdad1c33fc1d237")
+spotify_client_credentials_manager = SpotifyClientCredentials(client_id = "client_id",
+                                   client_secret = "client_secret")
 								   
 			
 #youtube
@@ -20,6 +20,6 @@ spotify_client_credentials_manager = SpotifyClientCredentials(client_id = "efb15
 from apiclient.discovery import build
 
 # arguments to be passed to build function
-YOUTUBE_DEVELOPER_KEY = "AIzaSyBHDCXMifi8vkHbcG79JIN0GlecpNkdf3c"
+YOUTUBE_DEVELOPER_KEY = "YOUTUBE_DEVELOPER_KEY"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"		
