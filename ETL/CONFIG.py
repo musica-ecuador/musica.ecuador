e8b8cce7689ebbb74260c00d7831a9824024467c
jcsuscriptor <jcsuscriptor@gmail.com>
2017-12-17 23:53:20 -0500

    Configurar para no subir claves

--- ETL/CONFIG.py
+++ ETL/CONFIG.py
@@ -4,8 +4,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 
 # arguments to be passed to build function
-
-
+ 
 PARSE_SERVER_HTTS = 0 #1 = is https 0 = http
 PARSE_SERVER =  "localhost:1337"
 PARSE_APPLICATION_ID = "123" 	
